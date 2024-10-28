d7549b6fa41ac8debbf666374673a0cdb0e67f0f
Madeline Luth <mluth@unomaha.edu>
2024-10-27 21:58:39 -0500

    starter widgets and extender page for alerts manager

--- glucoguide/lib/screens/alerts/alerts_page.dart
+++ glucoguide/lib/screens/alerts/alerts_page.dart
@@ -10,6 +10,51 @@ class AlertsPage extends StatefulWidget {
 class _AlertsPageState extends State<AlertsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Alerts Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to Page1
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Add_Alert()),
+                );
+              },
+              child: const Text('Add New Alert'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Page to handle making new alerts
+class Add_Alert extends StatelessWidget {
+  Add_Alert({super.key});
+  final _alertName = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('New Alert')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _alertName,
+              decoration: const InputDecoration(labelText: 'Title'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
