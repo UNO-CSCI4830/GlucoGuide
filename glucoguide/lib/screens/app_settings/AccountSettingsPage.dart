20d6eba0bbe774b574fadfb43d05568ef4ab925c
Miah407 <miahmason41@gmail.com>
2024-10-28 02:17:24 -0500

    Setting pages creation

---
+++ glucoguide/lib/screens/app_settings/AccountSettingsPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AccountSettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Account Settings'),
+      ),
+
+      // Account Settings page
+      body: Center(
+        child: const Text('Account Settings'),
+      ),
+    );
+  }
+}
--- glucoguide/lib/screens/app_settings/app_settings.dart
+++ glucoguide/lib/screens/app_settings/app_settings.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:glucoguide/screens/app_settings/AccountSettingsPage.dart';
+import 'package:glucoguide/screens/app_settings/NotificationSettingsPage.dart';
+import 'package:glucoguide/screens/login/login_page.dart';
 
 class AppSettingsPage extends StatefulWidget {
   const AppSettingsPage({super.key});
@@ -10,8 +14,59 @@ class AppSettingsPage extends StatefulWidget {
 class _AppSettingsPageState extends State<AppSettingsPage> {
   @override
   Widget build(BuildContext context) {
+    final List<SettingItem> settings = [
+      SettingItem(
+        title: 'Account',
+        icon: Icons.settings,
+        nextPage: AccountSettingsPage(),
+      ),
+      SettingItem(
+        title: 'Notifications',
+        icon: Icons.notifications,
+        nextPage: NotificationSettingsPage(),
+      ),
+      SettingItem(
+        title: 'Log Out',
+        icon: Icons.logout_rounded,
+        // make this secure
+        nextPage: LoginPage(),
+      ),
+    ];
+
     return Scaffold(
-      appBar: AppBar(title: const Text("App Settings")),
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView.builder(
+        itemCount: settings.length,
+        itemBuilder: (context, index) {
+          final setting = settings[index];
+          return ListTile(
+            leading: Icon(setting.icon),
+            title: Text(setting.title),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => setting.nextPage,
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
+}
+
+class SettingItem {
+  final String title;
+  final IconData icon;
+  final Widget nextPage;
+
+  SettingItem({
+    required this.title,
+    required this.icon,
+    required this.nextPage,
+  });
 }
