22af69c0081f9770cf0cec9e5b7ff40a1c8fc0a4
Miah407 <miahmason41@gmail.com>
2024-10-28 00:14:31 -0500

    Create link to settings page

--- glucoguide/lib/screens/home/home_page.dart
+++ glucoguide/lib/screens/home/home_page.dart
@@ -11,11 +11,40 @@ class HomePage extends StatelessWidget {
 
     final String name = user?.displayName ?? "User";
 
-    return Center(
-      child: const Text(
-        "Welcome to GlucoGuide",
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-      ),
+    return Row(
+      children: [
+        const Text('GlucoGuide', style: TextStyle(fontSize: 16)),
+        const Spacer(), // Pushes the button to the right
+        PopupMenuButton<int>(
+          icon: const Icon(Icons.more_vert),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(8.0),
+              bottomLeft: Radius.circular(8.0),
+            ),
+          ),
+          itemBuilder: (context) => [
+            PopupMenuItem(
+              value: 1,
+              child: const Text('Settings'),
+            ),
+            PopupMenuItem(
+              value: 2,
+              child: const Text('Other'),
+            ),
+          ],
+          onSelected: (value) {
+            // Handle the selected option
+            if (value == 1) {
+              Navigator.pushNamed(context, '/app_settings');
+            } else if (value == 2) {
+              // Do something for option 2
+            } else if (value == 3) {
+              // Do something for option 3
+            }
+          },
+        ),
+      ],
     );
   }
 }
