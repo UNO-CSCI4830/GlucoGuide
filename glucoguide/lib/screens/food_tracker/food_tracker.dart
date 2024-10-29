9217b2c3b8665aacede6824acc943d7cb01494e6
Emily <emilytran@unomaha.com>
2024-10-28 21:43:21 -0500

    Adding insulin calculator widget to food tracker page

--- glucoguide/lib/screens/food_tracker/food_tracker.dart
+++ glucoguide/lib/screens/food_tracker/food_tracker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:glucoguide/widgets/insulin_calculator.dart';
 
 class FoodTrackerPage extends StatefulWidget {
   const FoodTrackerPage({super.key});
@@ -10,6 +11,7 @@ class FoodTrackerPage extends StatefulWidget {
 class _FoodTrackerPageState extends State<FoodTrackerPage> {
   @override
   Widget build(BuildContext context) {
+    InsulinDoseCalculator();
     return const Placeholder();
   }
 }
