eb8009ebc4583b19cbd4cba5c36458b510abd1e5
Emily <emilytran@unomaha.com>
2024-10-28 21:15:59 -0500

    Insulin dose calculation logic

--- glucoguide/lib/widgets/insulin_calculator.dart
+++ glucoguide/lib/widgets/insulin_calculator.dart
@@ -1,3 +1,36 @@
 import 'package:flutter/material.dart';
 
-class InsulinDoseCalculator extends StatefulWidget {}
+class InsulinDoseCalculator extends StatefulWidget {
+  const InsulinDoseCalculator({super.key});
+
+  @override
+  State<InsulinDoseCalculator> createState() => _InsulinDoseCalculatorState();
+}
+
+class _InsulinDoseCalculatorState extends State<InsulinDoseCalculator> {
+  final TextEditingController glucoseController = TextEditingController();
+  final TextEditingController carbsController = TextEditingController();
+  final TextEditingController sensitivityFactorController =
+      TextEditingController(text: '50');
+  final TextEditingController carbRatioController =
+      TextEditingController(text: '10');
+  final TextEditingController targetGlucoseController =
+      TextEditingController(text: '100');
+
+  double insulinDose = 0.0;
+
+  void calculateDose() {
+    final glucose = double.tryParse(glucoseController.text) ?? 0.0;
+    final carbs = double.tryParse(carbsController.text) ?? 0.0;
+    final sensitivityFactor =
+        double.tryParse(sensitivityFactorController.text) ?? 50.0;
+    final carbRatio = double.tryParse(carbRatioController.text) ?? 10.0;
+    final targetGlucose =
+        double.tryParse(targetGlucoseController.text) ?? 100.0;
+
+    setState(() {
+      insulinDose =
+          ((glucose - targetGlucose) / sensitivityFactor) + (carbs / carbRatio);
+    });
+  }
+}
