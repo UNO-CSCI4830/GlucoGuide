10768d6b89ab525e11d9bef0d56eb70c218996e9
Emily <emtran.0401@gmail.com>
2024-11-26 13:20:02 -0600

    Added food item details to the nutrition service

--- glucoguide/lib/services/nutritionx_service.dart
+++ glucoguide/lib/services/nutritionx_service.dart
@@ -22,4 +22,22 @@ class NutritionixService {
       throw Exception('Failed to search for food: ${response.reasonPhrase}');
     }
   }
+
+  // Added a search for food item details
+  Future<Map<String, dynamic>> getFoodDetails(String itemId) async {
+    final url = Uri.parse(
+        'https://trackapi.nutritionix.com/v2/search/item?nix_item_id=$itemId');
+    final headers = {
+      'X-APP-ID': appId,
+      'X-APP-KEY': appKey,
+    };
+
+    final response = await http.get(url, headers: headers);
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to fetch food details: ${response.reasonPhrase}');
+    }
+  }
 }
